5131622f1dad94786b818862b9c3bbbb781f2938
caralemony <carolinelappleby@gmail.com>
2018-01-24 10:29:51 +0000

    timing feature complete

--- src/components/cards/index.js
+++ src/components/cards/index.js
@@ -1,39 +1,40 @@
-import React from 'react';
-import catty from "../../../public/cats.png"
+import React from "react";
+import catty from "../../../public/cats.png";
 
- export class Card extends React.Component {
+export class Card extends React.Component {
+  state = {
+    health: 50
+  };
 
-   state={
-     health: 50,
-   }
-   componentDidMount(){
+  componentDidMount = () => {
+    setInterval(() => {
+      this.setState(props => {
+        return { health: this.state.health - 1 };
+      });
+    }, 10000);
+  };
 
-       setInterval(function(){
-         this.setState((props)=>{
-         return {health: this.state.health-1}
-       }, 10000);
-     })
-
-   }
-
-    increment = () => {
-     this.setState((props) => {
-           return { health: this.state.health>=80 ? this.state.health=100 : this.state.health+20 };
-
-         })
-   }
-
+  increment = () => {
+    this.setState(props => {
+      return {
+        health:
+          this.state.health >= 80
+            ? (this.state.health = 100)
+            : this.state.health + 20
+      };
+    });
+  };
 
   render() {
-    return(
+    return (
       <div className="main-display">
-        <img src= {catty}></img>
+        <img src={catty} />
         <h1> great </h1>
         <span> {this.state.health} </span>
-        <div><button onClick={this.increment}>
-          click me
-        </button></div>
+        <div>
+          <button onClick={this.increment}>click me</button>
+        </div>
       </div>
-    )
+    );
   }
- }
+}
