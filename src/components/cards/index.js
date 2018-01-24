bd0a2495348c894473066c41f57acb5c5d171e9a
caralemony <carolinelappleby@gmail.com>
2018-01-24 12:10:55 +0000

    gifs added

--- src/components/cards/index.js
+++ src/components/cards/index.js
@@ -1,17 +1,23 @@
 import React from "react";
 import catty from "../../../public/cats.png";
+import hungryGif from "../../../public/hungry.gif";
+import pizza from "../../../public/pizza.gif";
+import mountain from "../../../public/mountain.gif";
 
 export class Card extends React.Component {
   state = {
-    health: 50
+    health: 100,
+    barColor: "green"
   };
 
   componentDidMount = () => {
     setInterval(() => {
       this.setState(props => {
-        return { health: this.state.health - 1 };
+        if (this.state.health > 0) {
+          return { health: this.state.health - 1 };
+        }
       });
-    }, 10000);
+    }, 500);
   };
 
   increment = () => {
@@ -25,28 +31,22 @@ export class Card extends React.Component {
     });
   };
 
-  // setBarWidth = {
-  //   width: "this.state.health",
-  //   height: "30px",
-  //   backgroundColor: "green"
-  // };
-
   render() {
     return (
       <div className="main-display">
-        <img src={catty} />
-        <h1> great </h1>
-        <span> {this.state.health} </span>
+        <img src={hungryGif} />
+        <h1>Give Pusheen Some Treats!</h1>
+        <span> {this.state.health} snacks </span>
         <div
           className="health_bar"
           style={{
             width: `${this.state.health}%`,
             height: "30px",
-            backgroundColor: "green"
+            backgroundColor: `${this.state.health > 50 ? "green" : "red"}`
           }}
         />
         <div>
-          <button onClick={this.increment}>click me</button>
+          <button onClick={this.increment}>Feed Me!</button>
         </div>
       </div>
     );
