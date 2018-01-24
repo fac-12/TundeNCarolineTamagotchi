9436e826238ec2fbab0e7735cab580e0aae58526
tundeaji1232 <ajiboye.tunde@gmail.com>
2018-01-24 12:59:45 +0000

    image switches

--- src/components/cards/index.js
+++ src/components/cards/index.js
@@ -3,6 +3,8 @@ import catty from "../../../public/cats.png";
 import hungryGif from "../../../public/hungry.gif";
 import pizza from "../../../public/pizza.gif";
 import mountain from "../../../public/mountain.gif";
+import donuts from "../../../public/donuts.png";
+
 
 export class Card extends React.Component {
   state = {
@@ -34,7 +36,7 @@ export class Card extends React.Component {
   render() {
     return (
       <div className="main-display">
-        <img src={hungryGif} />
+        <img src = {this.state.health > 50 ? pizza :hungryGif} />
         <h1>Give Pusheen Some Treats!</h1>
         <span> {this.state.health} snacks </span>
         <div
@@ -46,7 +48,7 @@ export class Card extends React.Component {
           }}
         />
         <div>
-          <button onClick={this.increment}>Feed Me!</button>
+          <button className="buttonFeed" onClick={this.increment}>Feed Me!</button>
         </div>
       </div>
     );
